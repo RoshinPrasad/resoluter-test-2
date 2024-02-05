3fc15bc3ca880ad6ad29822a48153507362863cb
unknown <roshinandalt275@gmail.com>
2024-02-05 09:11:58 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ProductList from './components/ProductList';
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className="App bg-gray-900 h-screen">
-      <header className="bg-gray-800 text-white text-center p-4 flex flex-row">
-        <h1 className="text-3xl font-semibold font-sans">TEST</h1>
-      </header>
+      <Navbar />
       <main>
         <ProductList />
       </main>
